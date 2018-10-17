d663799bb01d34b65808d6e12f2c52351fea21e1
Rosa Choe <rosachoe@berkeley.edu>
2018-10-16 19:50:26 -0700

    finished lecture 6

--- Lectures/Lecture6/plotting.py
+++ Lectures/Lecture6/plotting.py
@@ -1,6 +1,9 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.linear_model import LinearRegression
 
 np.random.seed(5)
 trainDataRange = np.array([-3,6])
@@ -113,20 +116,81 @@ def overfittingDemo(degrees = [0,1,2,3,4,5,7,10,13]):
         testError.append(currTestErr)
 
     #Plot the errors
-    plt.figure(figsize=(10,4))
-    plt.subplot(1,2,1)
-    plt.plot(degrees,trainError)
-    plt.ylabel("Error")
-    plt.xlabel("Degree of Model")
-    plt.title("Training Error")
+#     plt.figure(figsize=(10,4))
+#     plt.subplot(1,2,1)
+#     plt.plot(degrees,trainError)
+#     plt.ylabel("Error")
+#     plt.xlabel("Degree of Model")
+#     plt.title("Training Error")
 
 
-    plt.subplot(1,2,2)
-    plt.plot(degrees,np.log(testError))
-    plt.title("Test Error")
-    plt.xlabel("Degree of Model")
-    plt.ylabel("Log Error")
-    plt.show()
-    # Uncomment if you are curious about the actual error values
-    # print("Training Errors:",trainError)
-    # print("Test Errors:",testError)
+#     plt.subplot(1,2,2)
+#     plt.plot(degrees,np.log(testError))
+#     plt.title("Test Error")
+#     plt.xlabel("Degree of Model")
+#     plt.ylabel("Log Error")
+#     plt.show()
+#     Uncomment if you are curious about the actual error values
+#     print("Training Errors:",trainError)
+#     print("Test Errors:",testError)
+
+def get_features(data, col_list, y_name):
+    """
+    Function to return a numpy matrix of pandas dataframe features, given k column names and a single y column
+    Outputs X, a n X k dimensional numpy matrix, and Y, an n X 1 dimensional numpy matrix.
+    This is not a smart function - although it does drop rows with NA values. It might break. 
+    
+    data(DataFrame): e.g. mpg, mpg_train
+    col_list(list): list of columns to extract data from
+    y_name(string): name of the column you to treat as the y column
+    
+    Ideally returns one np.array of shape (len(data), len(col_list)), and one of shape (len(data), len(col_list))
+    """
+    
+    # keep track of numpy values
+    feature_matrix = data[col_list + [y_name]].dropna().values
+    return feature_matrix[:, :-1], feature_matrix[:, -1]    
+    
+def plot_multiple_linear_regression(data, x_1_name, x_2_name, y_name):
+    """
+    This function makes a 3D plot for multliple linear regression with two explanatory variables.
+    Thanks to: https://stackoverflow.com/a/26434204
+    
+    data(DataFrame): e.g. mpg, mpg_train
+    x_1_name(string): the name of the column representing the first explanatory variable
+    x_2_name(string): the name of the column representing the second explanatory variable
+    y_name(string): the name of the column representing the dependent/response variable
+    
+    returns None but outputs an interactive 3D plot
+    """
+    model = LinearRegression()
+    X, Y = get_features(data, [x_1_name, x_2_name], y_name)
+    fit = model.fit(X, Y)
+
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    x_surf = np.arange(0, max(data[x_1_name]), max(data[x_1_name])/10)                # generate a mesh
+    y_surf = np.arange(0, max(data[x_2_name]), max(data[x_2_name])/10)
+    x_surf, y_surf = np.meshgrid(x_surf, y_surf)
+
+    exog = pd.core.frame.DataFrame({x_1_name: x_surf.ravel(), x_2_name: y_surf.ravel()})
+    out = fit.predict(exog)
+    ax.plot_surface(x_surf, y_surf,
+                    out.reshape(x_surf.shape),
+                    rstride=1,
+                    cstride=1,
+                    color='None',
+                    alpha = 0.4)
+
+    ax.scatter(data[x_1_name], data[x_2_name], data[y_name],
+               marker='o',
+               alpha=1)
+
+    ax.set_xlabel(x_1_name)
+    ax.set_ylabel(x_2_name)
+    ax.set_zlabel(y_name)
+    plt.title("Linear Model with 2 Explanatory Variables: " + x_1_name + " and " + x_2_name + " vs. " + y_name, y=1.08, fontsize=16)
+
+    plt.show();
+    
